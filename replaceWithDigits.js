d1410a6a803d682112b6838ed85edad6e52f3c75
ahdabelle <83644296+ahdabelle@users.noreply.github.com>
2021-06-04 12:23:28 +0200

    Update replaceWithDigits.js

--- replaceWithDigits.js
+++ replaceWithDigits.js
@@ -2,61 +2,63 @@ String.prototype.replaceWithDigits = function(){
     
     if(lang == "de"){
         
-        var number = ["Eins", "Elf", "Einundzwanzig", "Einunddreiβig", "Einundvierzig", "Einundfünfzig", "Einundsechzig", "Einundsiebzig", "Einundachtzig", "Einundneunzig",
-            "Zwei", "Zwölf", "Zweiundzwanzig", "Zweiunddreiβig", "Zweiundvierzig", "Zweiundfünfzig", "Zweiundsechzig", "Zweiundsiebzig", "Zweiundachtzig", "Zweiundneunzig",
-            "Drei", "Dreizehn", "Dreiundzwanzig", "Dreiunddreiβig", "Dreiundvierzig", "Dreiundfünfzig", "Dreiundsechzig", "Dreiundsiebzig", "Dreiundachtzig", "Dreiundneunzig",
-            "Vier", "Vierzehn", "Vierundzwanzig", "Vierunddreiβig", "Vierundvierzig", "Vierundfünfzig", "Vierundsechzig", "Vierundsiebzig", "Vierundachtzig", "Vierundneunzig",
-            "Fünf", "Fünfzehn", "Fünfundzwanzig", "Fünfunddreiβig", "Fünfundvierzig", "Fünfundfünfzig", "Fünfundsechzig", "Fünfundsiebzig", "Fünfundachtzig", "Fünfundneunzig",
-            "Sechs", "Sechzehn", "Sechsundzwanzig", "Sechsunddreiβig", "Sechsundvierzig", "Sechsundfünfzig", "Sechsundsechzig", "Sechsundsiebzig", "Sechsundachtzig", "Sechsundneunzig",
-            "Sieben", "Siebzehn", "Siebenundzwanzig", "Siebenunddreiβig", "Siebenundvierzig", "Siebenundfünfzig", "Siebenundsechzig", "Siebenundsiebzig", "Siebenundachtzig", "Siebenundneunzig",
-            "Acht", "Achtzehn", "Achtundzwanzig", "Achtunddreiβig", "Achtundvierzig", "Achtundfünfzig", "Achtundsechzig", "Achtundsiebzig", "Achtundachtzig", "Achtundneunzig",
-            "Neun", "Neunzehn", "Neunundzwanzig", "Neununddreiβig", "Neunundvierzig", "Neunundfünfzig", "Neunundsechzig", "Neunundsiebzig", "Neunundachtzig", "Neunundneunzig",
-            "Zehn", "Zwanzig", "Dreiβig", "Vierzig", "Fünfzig", "Sechzig", "Siebzig", "Achtzig", "Neunzig", "Einhundert"
-        ];
-        
-    }else if(lang == "fr"){
-        
-        var number = ["Un", "Onze", "Vingt et un", "Trente et un", "Quarante et un", "Cinquante et un", "Soixante et un", "Soixante et onze", "Quatre-vingt-un", "Quatre-vingt-onze", 
-            "Deux", "Douze", "Vingt-deux", "Trente-deux", "Quarante-deux", "Cinquante-deux", "Soixante-deux", "Soixante-douze", "Quatre-vingt-deux", "Quatre-vingt-douze", 
-            "Trois", "Treize", "Vingt-trois", "Trente-trois", "Quarante-trois", "Cinquante-trois", "Soixante-trois", "Soixante-treize", "Quatre-vingt-trois", "Quatre-vingt-treize", 
-            "Quatre", "Quatorze", "Vingt-quatre", "Trente-quatre", "Quarante-quatre", "Cinquante-quatre", "Soixante-quatre", "Soixante-quatorze", "Quatre-vingt-quatre", "Quatre-vingt-quatorze", 
-            "Cinq", "Quinze", "Vingt-cinq", "Trente-cinq", "Quarante-cinq", "Cinquante-cinq", "Soixante-cinq", "Soixante-quinze", "Quatre-vingt-cinq", "Quatre-vingt-quinze", 
-            "Six", "Seize", "Vingt-six", "Trente-six", "Quarante-six", "Cinquante-six", "Soixante-six", "Soixante-six", "Quatre-vingt-six", "Quatre-vingt-seize", 
-            "Sept", "Dix-sept", "Vingt-sept", "Trente-sept", "Quarante-sept", "Cinquante-sept", "Soixante-sept", "Soixante-dix-sept", "Quatre-vingt-sept", "Quatre-vingt-dix-sept", 
-            "Huit", "Dix-huit", "Vingt-huit", "Trente-huit", "Fo rty-huit","Cinquante-huit"," Soixante-huit","Soixante-dix-huit","Quatre-vingt-huit"," Quatre-vingt-dix-huit",
-            "Neuf","Dix-neuf"," Vingt-neuf "," Trente-neuf ", "Quarante-neuf","Cinquante-neuf","Soixante-neuf","Soixante-dix-neuf ","Quatre-vingt-neuf"," Quatre-vingt-dix-neuf",
-            "Dix","Vingt","Trente","Quarante","Cinquante","Soixante","Soixante-dix","Quatre-vingt","Quatre-vingt-dix","Cent"
-        ];
-        
-        
-    }else{
-        
-        var number = ["One", "Eleven", "Twenty-one", "Thirty-one", "Forty-one", "Fifty-one", "Sixty-one", "Seventy-one", "Eighty-one", "Ninety-one",
-            "Two", "Twelve", "Twenty-two", "Thirty-two", "Forty-two", "Fifty-two", "Sixty-two", "Seventy-two", "Eighty-two", "Ninety-two",
-            "Three", "Thirteen", "Twenty-three", "Thirty-three", "Forty-three", "Fifty-three", "Sixty-three", "Seventy-three", "Eighty-three", "Ninety-three",
-            "Four", "Fourteen", "Twenty-four", "Thirty-four", "Forty-four", "Fifty-four", "Sixty-four", "Seventy-four", "Eighty-four", "Ninety-four",
-            "Five", "Fifteen", "Twenty-five", "Thirty-five", "Forty-five", "Fifty-five", "Sixty-five", "Seventy-five", "Eighty-five", "Ninety-five",
-            "Six", "Sixteen", "Twenty-six", "Thirty-six", "Forty-six", "Fifty-six", "Sixty-six", "Seventy-six", "Eighty-six", "Ninety-six",
-            "Seven", "Seventeen", "Twenty-seven", "Thirty-seven", "Forty-seven", "Fifty-seven", "Sixty-seven", "Seventy-seven", "Eighty-seven", "Ninety-seven",
-            "Eight", "Eighteen", "Twenty-eight", "Thirty-eight", "Forty-eight", "Fifty-eight", "Sixty-eight", "Seventy-eight", "Eighty-eight", "Ninety-eight",
-            "Nine", "Nineteen", "Twenty-nine", "Thirty-nine", "Forty-nine", "Fifty-nine", "Sixty-nine", "Seventy-nine", "Eighty-nine", "Ninety-nine",
-            "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "One Hundred"
+        var number = ["Neunundneunzig","Neunundachtzig","Neunundsiebzig","Neunundsechzig","Neunundfünfzig","Neunundvierzig","Neununddreiβig","Neunundzwanzig","Neunzehn",
+            "Achtundneunzig","Achtundachtzig","Achtundsiebzig","Achtundsechzig","Achtundfünfzig","Achtundvierzig","Achtunddreiβig","Achtundzwanzig","Achtzehn",
+            "Siebenundneunzig","Siebenundachtzig","Siebenundsiebzig","Siebenundsechzig","Siebenundfünfzig","Siebenundvierzig","Siebenunddreiβig","Siebenundzwanzig","Siebzehn",
+            "Sechsundneunzig","Sechsundachtzig","Sechsundsiebzig","Sechsundsechzig","Sechsundfünfzig","Sechsundvierzig","Sechsunddreiβig","Sechsundzwanzig","Sechzehn",
+            "Fünfundneunzig","Fünfundachtzig","Fünfundsiebzig","Fünfundsechzig","Fünfundfünfzig","Fünfundvierzig","Fünfunddreiβig","Fünfundzwanzig","Fünfzehn",
+            "Vierundneunzig","Vierundachtzig","Vierundsiebzig","Vierundsechzig","Vierundfünfzig","Vierundvierzig","Vierunddreiβig","Vierundzwanzig","Vierzehn",
+            "Dreiundneunzig","Dreiundachtzig","Dreiundsiebzig","Dreiundsechzig","Dreiundfünfzig","Dreiundvierzig","Dreiunddreiβig","Dreiundzwanzig","Dreizehn",
+            "Zweiundneunzig","Zweiundachtzig","Zweiundsiebzig","Zweiundsechzig","Zweiundfünfzig","Zweiundvierzig","Zweiunddreiβig","Zweiundzwanzig","Zwölf",
+            "Einundneunzig","Einundachtzig","Einundsiebzig","Einundsechzig","Einundfünfzig","Einundvierzig","Einunddreiβig","Einundzwanzig","Elf",
+            "Einhundert","Neunzig","Achtzig","Siebzig","Sechzig","Fünfzig","Vierzig","Dreiβig","Zwanzig","Zehn","Neun","Acht","Sieben","Sechs","Fünf","Vier","Drei","Zwei","Eins"
         ];
 
         
+    }else if(lang == "fr"){
+        
+        var number = ["Quatre-vingt-dix-neuf","Quatre-vingt-neuf","Soixante-dix-neuf ","Soixante-neuf","Cinquante-neuf","Quarante-neuf"," Trente-neuf "," Vingt-neuf ","Dix-neuf",
+            "Quatre-vingt-dix-huit","Quatre-vingt-huit", "Soixante-dix-huit"," Soixante-huit","Cinquante-huit","Quarante-huit","Trente-huit","Vingt-huit","Dix-huit",
+            "Quatre-vingt-dix-sept","Quatre-vingt-sept","Soixante-dix-sept","Soixante-sept","Cinquante-sept","Quarante-sept","Trente-sept","Vingt-sept","Dix-sept",
+            "Quatre-vingt-seize","Quatre-vingt-six","Soixante-six","Soixante-six","Cinquante-six","Quarante-six","Trente-six","Vingt-six","Seize",
+            "Quatre-vingt-quinze","Quatre-vingt-cinq","Soixante-quinze","Soixante-cinq","Cinquante-cinq","Quarante-cinq","Trente-cinq","Vingt-cinq","Quinze",
+            "Quatre-vingt-quatorze","Quatre-vingt-quatre","Soixante-quatorze","Soixante-quatre","Cinquante-quatre","Quarante-quatre","Trente-quatre","Vingt-quatre","Quatorze",
+            "Quatre-vingt-treize","Quatre-vingt-trois","Soixante-treize","Soixante-trois","Cinquante-trois","Quarante-trois","Trente-trois","Vingt-trois","Treize",
+            "Quatre-vingt-douze","Quatre-vingt-deux","Soixante-douze","Soixante-deux","Cinquante-deux","Quarante-deux","Trente-deux","Vingt-deux","Douze",
+            "Quatre-vingt-onze","Quatre-vingt-un","Soixante et onze","Soixante et un","Cinquante et un","Quarante et un","Trente et un","Vingt et un","Onze",
+            "Cent","Quatre-vingt-dix","Quatre-vingt","Soixante-dix","Soixante","Cinquante","Quarante","Trente","Vingt","Dix",
+            "Neuf","Huit","Sept","Six","Cinq","Quatre","Trois","Deux","Un"
+        ];
+        
+    }else{
+        
+        var number = ["Ninety-nine","Eighty-nine","Seventy-nine","Sixty-nine","Fifty-nine","Forty-nine","Thirty-nine","Twenty-nine","Nineteen",
+            "Ninety-eight","Eighty-eight","Seventy-eight","Sixty-eight","Fifty-eight","Forty-eight","Thirty-eight","Twenty-eight","Eighteen",
+            "Ninety-seven","Eighty-seven","Seventy-seven","Sixty-seven","Fifty-seven","Forty-seven","Thirty-seven","Twenty-seven","Seventeen",
+            "Ninety-six","Eighty-six","Seventy-six","Sixty-six","Fifty-six","Forty-six","Thirty-six","Twenty-six","Sixteen",
+            "Ninety-five","Eighty-five","Seventy-five","Sixty-five","Fifty-five","Forty-five","Thirty-five","Twenty-five","Fifteen",
+            "Ninety-four","Eighty-four","Seventy-four","Sixty-four","Fifty-four","Forty-four","Thirty-four","Twenty-four","Fourteen",
+            "Ninety-three","Eighty-three","Seventy-three","Sixty-three","Fifty-three","Forty-three","Thirty-three","Twenty-three","Thirteen",
+            "Ninety-two","Eighty-two","Seventy-two","Sixty-two","Fifty-two","Forty-two","Thirty-two","Twenty-two","Twelve",
+            "Ninety-one","Eighty-one","Seventy-one","Sixty-one","Fifty-one","Forty-one","Thirty-one","Twenty-one","Eleven",
+            "One Hundred","Ninety","Eighty","Seventy","Sixty","Fifty","Forty","Thirty","Twenty","Ten",
+            "Nine","Eight","Seven","Six","Five","Four","Three","Two","One"
+        ];
+        
     }
     
-    var digit = ["1", "11", "21", "31", "41", "51", "61", "71", "81", "91",
-            "2", "12", "22", "32", "42", "52", "62", "72", "82", "92",
-            "3", "13", "23", "33", "43", "53", "63", "73", "83", "93",
-            "4", "14", "24", "34", "44", "54", "64", "74", "84", "94",
-            "5", "15", "25", "35", "45", "55", "65", "75", "85", "95",
-            "6", "16", "26", "36", "46", "56", "66", "76", "86", "96",
-            "7", "17", "27", "37", "47", "57", "67", "77", "87", "97",
-            "8", "18", "28", "38", "48", "58", "68", "78", "88", "98",
-            "9", "19", "29", "39", "49", "59", "69", "79", "89", "99",
-        	"10", "20", "30", "40", "50", "60", "70", "80", "90", "100"
-        ];
+    var digit = ["99","89","79","69","59","49","39","29","19",
+        "98","88","78","68","58","48","38","28","18",
+        "97","87","77","67","57","47","37","27","17",
+        "96","86","76","66","56","46","36","26","16",
+        "95","85","75","65","55","45","35","25","15",
+        "94","84","74","64","54","44","34","24","14",
+        "93","83","73","63","53","43","33","23","13",
+        "92","82","72","62","52","42","32","22","12",
+        "91","81","71","61","51","41","31","21","11",
+        "100","90","80","70","60","50","40","30","20","10",
+        "9","8","7","6","5","4","3","2","1"
+    ];
     
     var str = this;
     for(var i = 0; i < number.length; i++){
